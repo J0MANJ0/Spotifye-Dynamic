abf56560f21110b55b14c9a525be08daa9ab415d
Joseph Mark <josephmarkmay@gmail.com>
2026-01-10 00:26:05 +0300

    updates

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,17 +7,17 @@ import { SOC_INIT } from './socket';
 
 const server = createServer(app);
 
-SOC_INIT.INIT_SOCKET(server);
+if (ENV.ENABLE_SOCKETS === 'true') {
+  SOC_INIT.INIT_SOCKET(server);
+}
 
 connectDB()
   .then(() => {
     const { PORT } = ENV;
-    server.listen(PORT, () =>
-      logger.info(`Socket running on ws://localhost:${PORT}/socket.io`)
-    );
-    app.listen(PORT, () =>
-      logger.info(`Server running on http://localhost:${PORT}`)
-    );
+    server.listen(PORT, () => {
+      logger.info(`Socket running on ws://localhost:${PORT}/socket.io`);
+      logger.info(`Server running on http://localhost:${PORT}`);
+    });
   })
   .catch((err) => {
     logger.error('Failed to start server/connect db:', err);
